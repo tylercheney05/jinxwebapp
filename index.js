85072c4fc46cbc19b12e72949d3fc044cc45c6aa
Tyler Cheney <tyler.cheney.05@gmail.com>
2024-10-31 12:02:52 -0700

    move environment variables to root

--- index.js
+++ index.js
@@ -152,6 +152,6 @@ app.get("*", (req, res) => {
   return res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
 })
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PROXY_PORT || 5000
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
